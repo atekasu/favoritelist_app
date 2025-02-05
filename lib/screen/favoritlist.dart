6ed1dda1e0c2bed8cd415b479ddfa136190f4d2f
atekasu <ataranaize@gmail.com>
2025-02-05 22:35:36 +0900

    ain画面

--- lib/screen/favoritlist.dart
+++ lib/screen/favoritlist.dart
@@ -1,34 +1,37 @@
+import 'package:alpah/favoriteplace/favorite_place.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:riverpod/riverpod.dart';
 import 'package:alpah/screen/addfavaritomodel.dart';
 
-class FavoritlistScreen extends StatefulWidget {
-  const FavoritlistScreen({
-    super.key,
-  });
+class FavoritelistscreenState extends ConsumerWidget {
   @override
-  State<FavoritlistScreen> createState() => _FavoritelistscreenState();
-}
-
-class _FavoritelistscreenState extends State<FavoritlistScreen> {
-  @override
-  Widget build(BuildContext) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final favoritelist = ref.watch(favoritelistProvide);
     return Scaffold(
-      appBar: AppBar(title: Text('favoritelist'),
-       actions: [
-        IconButton(
-          icon: const Icon(Icons.add),
-          onPressed: () {
-            Navigator.of(context)
-                .push(MaterialPageRoute(builder: (ctx) =>  AddScreen()));
-          },
-        ),
-      ],
-),
-      body: Center(
-        child: Text('data'),
+      appBar: AppBar(
+        title: Text('favoritelist'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.add),
+            onPressed: () {
+              Navigator.of(context)
+                  .push(MaterialPageRoute(builder: (ctx) => AddScreen()));
+            },
+          ),
+        ],
       ),
+      body: favoritelist.isEmpty
+          ? Center(
+              child: Text('登録してください'),
+            )
+          : ListView.builder(
+              itemCount: favoritelist.length,
+              itemBuilder: (ctx, index) => ListTile(
+                title: Text(favoritelist[index].title),
+              ),
+            ),
     );
   }
 }
